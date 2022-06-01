42e7ea44edc3da893b66f780bb71b7bff631f979
Cristhian Morales <camoralesh27@gmail.com>
2022-05-31 18:46:46 -0600

    imprimiendo en consola un ciclo for

--- main.js
+++ main.js
@@ -1,8 +1,9 @@
 var colors = require(`colors`);
 console.log("Hola desde consola".rainbow);
 
-cont = 0;
-while(cont = 6){
-    
-    console.log("5x1"
+for(let i=0; i<=10; i++ ){
+    console.log("Esto me encanta".rainbow)
+
+    //let mult= 5 * i;
+    //console.log("5 x ".rainbow + i + " es:".rainbow + mult); 
 }
